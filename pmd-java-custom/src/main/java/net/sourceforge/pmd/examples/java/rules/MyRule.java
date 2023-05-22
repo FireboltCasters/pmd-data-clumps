3d867f8374bb1ddafe271432396b8b75d4e07460
Nils <nilsbaumgartner1994@gmail.com>
2023-05-23 01:35:57 +0200

    Added file writer

--- pmd-java-custom/src/main/java/net/sourceforge/pmd/examples/java/rules/MyRule.java
+++ pmd-java-custom/src/main/java/net/sourceforge/pmd/examples/java/rules/MyRule.java
@@ -8,12 +8,17 @@ import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 import net.sourceforge.pmd.properties.StringProperty;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 
 public class MyRule extends AbstractJavaRule {
 
     static int count = 0;
+    static String output = "";
 
     private static final StringProperty BAD_NAME = StringProperty.named("badName")
             .defaultValue("foo")
@@ -41,13 +46,38 @@ public class MyRule extends AbstractJavaRule {
 
     public Object visit(ASTClassOrInterfaceDeclaration node, Object data) {
         String className = node.getCanonicalName();
-        System.out.println(count+" - "+className);
-        ASTClassOrInterfaceType superClass = node.getSuperClassTypeNode();
-        if(superClass!=null){
-            System.out.println("extends: "+superClass.getSimpleName());
+        String simpleName = node.getSimpleName();
+        String outputRow = "\t\""+className+"\": \""+simpleName+"\",\n";
+        System.out.println(outputRow);
+
+        //TODO Add this row to the output file saved on the desktop
+
+        try {
+            // Create file
+            String desktopPath = System.getProperty("user.home") + "/Desktop/";
+            File outputFile = new File(desktopPath + "pmd-data-clumps-classAndInterfaces.csv");
+
+            // If file does not exist, create it
+            if (!outputFile.exists()) {
+                outputFile.createNewFile();
+            }
+
+            // Create FileWriter and BufferedWriter (in append mode)
+            FileWriter fileWriter = new FileWriter(outputFile, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            // Write the outputRow to the file
+            bufferedWriter.write(outputRow);
+
+            // Always close writers
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        MyRule.count++;
-        return null;
+
+//        System.out.println(count+" - "+className);
+//        MyRule.count++;
+        return super.visit(node, data);
     }
 }
