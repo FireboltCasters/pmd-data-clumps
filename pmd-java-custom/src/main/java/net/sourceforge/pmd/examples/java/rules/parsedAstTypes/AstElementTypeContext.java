eadf292e31c4fcab2dc16b1cb368c4bc5e819ccb
Nils <nilsbaumgartner1994@gmail.com>
2023-05-23 02:04:31 +0200

    Added ParsedTypes

---
+++ pmd-java-custom/src/main/java/net/sourceforge/pmd/examples/java/rules/parsedAstTypes/AstElementTypeContext.java
@@ -0,0 +1,12 @@
+package net.sourceforge.pmd.examples.java.rules.parsedAstTypes;
+
+public class AstElementTypeContext {
+    public String name;
+    public String key;
+    public String type;
+    public AstPosition position;
+
+    public AstElementTypeContext(){
+
+    }
+}
---
+++ pmd-java-custom/src/main/java/net/sourceforge/pmd/examples/java/rules/parsedAstTypes/ClassOrInterfaceTypeContext.java
@@ -0,0 +1,25 @@
+package net.sourceforge.pmd.examples.java.rules.parsedAstTypes;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ClassOrInterfaceTypeContext extends AstElementTypeContext {
+    public List<String> modifiers = new ArrayList<String>();
+    public Map<String, MemberFieldParameterTypeContext> fields = new HashMap<String, MemberFieldParameterTypeContext>();
+    public Map<String, MethodTypeContext> methods = new HashMap<String, MethodTypeContext>();
+    public String fileKey;
+    public String anonymous;
+    public List<String> implements_ = new ArrayList<String>();
+    public List<String> extends_ = new ArrayList<String>();
+
+    public String definedInClassOrInterfaceTypeKey;
+
+    public Map<String, ClassOrInterfaceTypeContext> innerDefinedClasses = new HashMap<String, ClassOrInterfaceTypeContext>();
+    public Map<String, ClassOrInterfaceTypeContext> innerDefinedInterfaces = new HashMap<String, ClassOrInterfaceTypeContext>();
+
+    public ClassOrInterfaceTypeContext(){
+
+    }
+}
